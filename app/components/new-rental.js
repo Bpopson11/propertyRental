fc878b30dbf33dee6138725947ea67673f4114f4
Afton Downey and Bri Popson <aaike001@gmail.com>
2016-03-29 08:37:00 -0700

    remove annoucement functionality to return to mvp

--- app/components/new-rental.js
+++ app/components/new-rental.js
@@ -7,15 +7,15 @@ export default Ember.Component.extend({
       this.set('addNewRental', true);
     },
     save1() {
-  var params = {
-    owner: this.get('owner'),
-    city: this.get('city'),
-    type: this.get('type'),
-    image: this.get('image'),
-    bedrooms: this.get('bedrooms'),
-  };
-  this.set('addNewRental', false),
-  this.sendAction('save2', params);
-}
+      var params = {
+        owner: this.get('owner'),
+        city: this.get('city'),
+        type: this.get('type'),
+        image: this.get('image'),
+        bedrooms: this.get('bedrooms'),
+      };
+      this.set('addNewRental', false),
+      this.sendAction('save2', params);
+    }
   }
 });
