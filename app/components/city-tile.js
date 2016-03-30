5a4f52cbfc3ef9d152249c8d8227f177473a2fa9
Afton Downey and Bri Popson <bpopson@gmail.com>
2016-03-30 11:51:23 -0700

    still need to list rentals on city page, not currently working

--- app/components/city-tile.js
+++ app/components/city-tile.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+
+    findRentals(rental, city) {
+      var rental_list = [];
+      forEach()
+      if(rental.city.name === this.city.name) {
+        rental_list.push(rental)
+      }
+    }
+  }
 });
